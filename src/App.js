0c333e393551827b476807f285a0650c83a1d98e
JhonZuluaga007 <jhonsalazar93@hotmail.com>
2022-07-08 02:51:01 -0500

    add initial integration with rickandmortyapi service

--- src/App.js
+++ src/App.js
@@ -1,23 +1,85 @@
-import logo from './logo.svg';
 import './App.css';
+import { gql, useQuery } from '@apollo/client';
+
+const GET_CHARACTERS = gql`
+  query  getCharacters {
+    characters {
+      results {
+      id
+      name
+      status
+      species
+      type
+      gender
+      origin{
+        id
+        name
+      }
+      location{
+        id
+        name
+      }
+      image
+      created
+    }}
+  }
+`;
+
+const GET_CHARACTER = (id) => {
+
+const queryString = `query  getCharacter {
+    character(id: ${id} ) {
+     
+      id
+      name
+      status
+      species
+      type
+      gender
+      origin{
+        id
+        name
+      }
+      location{
+        id
+        name
+      }
+      image
+      created
+    }
+  }
+`
+  return gql(queryString);
+};
+
+function getRandomInt() {
+  return Math.floor(Math.random() * 826);
+}
+
+function DisplayCharacter() {
+  //const { loading, error, data } = useQuery(GET_CHARACTERS);
+  console.log(getRandomInt());
+  const { loading, error, data } = useQuery(GET_CHARACTER(getRandomInt()));
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :</p>;
+  //return data.characters.results.map( result => {
+    return <div key={data.character.id}>
+      <h3>{data.character.name}</h3>
+      <img width="400" height="250" alt="location-reference" src={`${data.character.image}`} />
+      <br />
+      <b>About this location:</b>
+      <p>{data.character.species}</p>
+      <br />
+    </div>
+//});
+}
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <DisplayCharacter />
     </div>
   );
 }
