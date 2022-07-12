b4305b4d10841da4078b8d027a61370e0da264ba
JhonZuluaga007 <jhonsalazar93@hotmail.com>
2022-07-11 23:14:48 -0500

    a component is created for the management of the characters and the history of the characters

--- src/App.js
+++ src/App.js
@@ -1,106 +1,34 @@
 import './App.css';
-import { gql, useLazyQuery } from '@apollo/client';
-//import { GET_CHARACTER } from './apollo/queries';
-//import { getRandomInt } from './utils/random.utils';
-import { useState } from 'react';
+import { useLazyQuery } from '@apollo/client';
+import { GET_CHARACTER } from './apollo/queries';
+import Character from './components/character';
+import ListCharacters from './components/list-character';
 
-
-const GET_CHARACTER = gql(
-  `query  getCharacter($idCharacter : ID!) {
-    character(id: $idCharacter ) {
-      id
-      name
-      status
-      species
-      type
-      gender
-      origin{
-        id
-        name
-      }
-      location{
-        id
-        name
-      }
-      image
-      created
-    }
-  }
-  `)
-
-
-
-function DisplayCharacter(props) {
-  
-  
+export function DisplayCharacter(props) {
   const getRandomInt = Math.floor(Math.random() * 826);
-  console.log(getRandomInt);
-
   const [getCharacter, { loading, called, data }] = useLazyQuery(GET_CHARACTER);
   
   const manejarEnvio = newcharts => {
     newcharts.preventDefault();
-      props.onSubmit(newcharts);
+    getCharacter({variables: {idCharacter: getRandomInt}});
+    props.onSubmit(data.character);
   }
     if (called && loading) return <p>Loading ...</p>
     if (!called) {
       return <>
       <h2>No se ha cargado ningún personaje</h2>
-      <button onSubmit={manejarEnvio} onClick={() => getCharacter({variables: {idCharacter: getRandomInt}})}>Load greeting</button>
+      <button onClick={manejarEnvio}>Load greeting</button>
       </>
     }
     return <> <Character character={data?.character} />
-          <button onSubmit={manejarEnvio} onClick={() => getCharacter({variables: {idCharacter: getRandomInt}})}>Load greeting</button></>
-}
-
-function DisplayCharacters () {
-  const [characters, setCharacters] = useState([]);
-
-  const agregarTarea = character => {
-
-    console.log(character);
-    //if (character.name.trim()) {
-      //character.name = character.name.trim();
-      //const updatedCharacters = [character, ...characters];
-      //setCharacters(updatedCharacters);
-    //}
-  }
-
-  return (
-    <>
-      <DisplayCharacter onSubmit={agregarTarea} />
-      <div className='tareas-lista-contenedor'>
-        {
-          characters.map((character)=>
-            <Character key={character.id} character={character} />
-          )
-        }
-      </div>
-    </>
-
-  );
-
-}
-
-function Character({ character}) {
-  if(character ===null) return null
-  return (
-    <div key={character.id}>
-      <h3>{character?.name}</h3>
-      <img width="400" height="250" alt="location-reference" src={`${character.image}`} />
-      <br />
-      <b>About this location:</b>
-      <p>{character.species}</p>
-      <br />
-    </div>
-  );    
+          <button onClick={manejarEnvio}>Load greeting</button></>
 }
 
 
 function App() {
   return (
     <div className="App">
-      <DisplayCharacters />
+      <ListCharacters />
     </div>
   );
 }
